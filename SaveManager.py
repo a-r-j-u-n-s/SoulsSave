20a1600ec459555617e727f4866bb6460a005977
ARJUN SRIVASTAVA <arjunsrivastava01@gmail.com>
2022-02-04 01:16:08 -0800

    added process function

--- SaveManager.py
+++ SaveManager.py
@@ -1,4 +1,26 @@
+import os
+import psutil
+
 class SaveManager:
+    defaultPath = 'Path/to/save'
+
+    def __init__(self):
+        pass
 
     class Save:
         pass
+
+    @staticmethod
+    def check_process_running(process_name):
+        """
+        Check if there is any running process that contains the given name processName.
+        """
+        # Iterate over the all the running process
+        for proc in psutil.process_iter():
+            try:
+                # Check if process name contains the given name string.
+                if process_name.lower() in proc.name().lower():
+                    return True
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        return False
