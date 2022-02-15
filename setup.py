ade47dc7662d84378737f1db1953ff4366d11e6e
ARJUN SRIVASTAVA <arjunsrivastava01@gmail.com>
2022-02-15 06:26:40 -0800

    added setup.py for generating package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='savegame-manager',
+    version='1.0.0',
+    scripts=['./scripts/savegame.cmd'],
+    author='Arjun Srivastava',
+    description='Save folder manager for PC games.',
+    packages=['lib.savegame'],
+    install_requires=[
+        'setuptools',
+        'argparse',
+        'psutil>=5.7.0'
+
+    ],
+    python_requires='>=3.8'
+)
